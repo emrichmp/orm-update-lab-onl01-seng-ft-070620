a1a5c17789c82741bb6c8df8ee51c6ce4d3f454a
Emrich-Michael Perrier <emrichmp@gmail.com>
2020-08-19 18:22:54 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -24,4 +24,28 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.drop_table
+    
+  end
+  
+  def save
+    
+  end
+  
+  def self.create
+    
+  end
+  
+  def self.new_from_db
+    
+  end
+  
+  def self.find_by_name
+    
+  end
+  
+  def update
+    
+  end
+  
 end
